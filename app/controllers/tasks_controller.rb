ff19ae6479886f1eacc1205f274206eef05e8dc5
rufunt <jazzoo@mail.ru>
2018-07-01 16:19:17 +0300

    Add Complete Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,16 @@
 class TasksController < ApplicationController
   
   before_action :set_project
+  before_action :set_task, except: [:create]
   
   def create
     @task = @project.tasks.create(task_params)
     redirect_to @project
+  end
+
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
   end
 
   def destroy
@@ -23,6 +29,10 @@ class TasksController < ApplicationController
     @project = Project.find(params[:project_id])
   end
   
+  def set_task
+    @task = @project.tasks.find(params[:id])
+  end
+
   def task_params
     params[:task].permit(:content)
   end
